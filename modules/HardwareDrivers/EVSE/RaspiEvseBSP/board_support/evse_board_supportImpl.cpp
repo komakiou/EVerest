f2ff0f066d1d0b83c0eec7f89fdc36199052317c
Christopher Simon <christopher.cs.simon@gmail.com>
2026-04-30 20:08:59 +0800

    Added Test Code for Publishing Topics from a Thread

--- modules/HardwareDrivers/EVSE/RaspiEvseBSP/board_support/evse_board_supportImpl.cpp
+++ modules/HardwareDrivers/EVSE/RaspiEvseBSP/board_support/evse_board_supportImpl.cpp
@@ -3,6 +3,8 @@
 
 #include "evse_board_supportImpl.hpp"
 
+#include <chrono>
+
 namespace module {
 namespace board_support {
 
@@ -10,38 +12,59 @@ void evse_board_supportImpl::init() {
 }
 
 void evse_board_supportImpl::ready() {
+    i2c_read_thread_handle = std::thread(&evse_board_supportImpl::i2c_read_thread, this);
 }
 
 void evse_board_supportImpl::handle_enable(bool& value) {
     // your code for cmd enable goes here
+    EVLOG_info << "evse_board_supportImpl::handle_enable " << value;
 }
 
 void evse_board_supportImpl::handle_pwm_on(double& value) {
     // your code for cmd pwm_on goes here
+    EVLOG_info << "evse_board_supportImpl::handle_pwm_on " << value;
 }
 
 void evse_board_supportImpl::handle_cp_state_X1() {
     // your code for cmd cp_state_X1 goes here
+    EVLOG_info << "evse_board_supportImpl::handle_cp_state_X1()";
 }
 
 void evse_board_supportImpl::handle_cp_state_F() {
     // your code for cmd cp_state_F goes here
+    EVLOG_info << "evse_board_supportImpl::handle_cp_state_F()";
 }
 
 void evse_board_supportImpl::handle_cp_state_E() {
     // your code for cmd cp_state_E goes here
+    EVLOG_info << "evse_board_supportImpl::handle_cp_state_E()";
 }
 
 void evse_board_supportImpl::handle_allow_power_on(types::evse_board_support::PowerOnOff& value) {
     // your code for cmd allow_power_on goes here
+    EVLOG_info << "evse_board_supportImpl::handle_allow_power_on";
 }
 
 void evse_board_supportImpl::handle_ac_switch_three_phases_while_charging(bool& value) {
     // your code for cmd ac_switch_three_phases_while_charging goes here
+    EVLOG_info << "evse_board_supportImpl::handle_ac_switch_three_phases_while_charging " << value;
 }
 
 void evse_board_supportImpl::handle_ac_set_overcurrent_limit_A(double& value) {
     // your code for cmd ac_set_overcurrent_limit_A goes here
+    EVLOG_info << "evse_board_supportImpl::handle_ac_set_overcurrent_limit_A " << value;
+}
+
+void evse_board_supportImpl::i2c_read_thread(){
+    
+    for(;;)
+    {
+        sleep(1);
+
+        types::board_support_common::BspEvent event;
+        event.event = types::board_support_common::Event::A;
+        publish_event(event);
+    }
 }
 
 } // namespace board_support
--- modules/HardwareDrivers/EVSE/RaspiEvseBSP/board_support/evse_board_supportImpl.hpp
+++ modules/HardwareDrivers/EVSE/RaspiEvseBSP/board_support/evse_board_supportImpl.hpp
@@ -13,7 +13,7 @@
 #include "../RaspiEvseBSP.hpp"
 
 // ev@75ac1216-19eb-4182-a85c-820f1fc2c091:v1
-// insert your custom include headers here
+#include <utils/thread.hpp>
 // ev@75ac1216-19eb-4182-a85c-820f1fc2c091:v1
 
 namespace module {
@@ -54,7 +54,8 @@ private:
     virtual void ready() override;
 
     // ev@3370e4dd-95f4-47a9-aaec-ea76f34a66c9:v1
-    // insert your private definitions here
+    Everest::Thread i2c_read_thread_handle;
+    void i2c_read_thread();
     // ev@3370e4dd-95f4-47a9-aaec-ea76f34a66c9:v1
 };
 
