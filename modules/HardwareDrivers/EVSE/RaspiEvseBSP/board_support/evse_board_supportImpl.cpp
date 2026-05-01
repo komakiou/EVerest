d2565641726422169b43ccd7af1a885e4d2995a1
Christopher Simon <christopher.cs.simon@gmail.com>
2026-05-01 18:37:54 +0800

    Added function to open I2C interface

--- modules/HardwareDrivers/EVSE/RaspiEvseBSP/board_support/evse_board_supportImpl.cpp
+++ modules/HardwareDrivers/EVSE/RaspiEvseBSP/board_support/evse_board_supportImpl.cpp
@@ -4,11 +4,52 @@
 #include "evse_board_supportImpl.hpp"
 
 #include <chrono>
+#include <sys/ioctl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+/**
+ * i2c_open() - Open an I2C bus and bind it to a slave address.
+ *
+ * @bus_path:  Path to the I2C bus device, e.g. "/dev/i2c-1"
+ * @addr:      7-bit I2C slave address
+ *
+ * Returns a file descriptor >= 0 on success, -1 on failure.
+ */
+static int i2c_open(const char *bus_path, uint8_t addr)
+{
+    int fd = open(bus_path, O_RDWR);
+    if (fd < 0) {
+        perror("i2c_open: open");
+        return -1;
+    }
+ 
+    /* Tell the kernel which slave device we want to talk to */
+    if (ioctl(fd, I2C_SLAVE, addr) < 0) {
+        perror("i2c_open: ioctl I2C_SLAVE");
+        close(fd);
+        return -1;
+    }
+ 
+    return fd;
+}
+
 
 namespace module {
 namespace board_support {
 
 void evse_board_supportImpl::init() {
+    
+    fd = i2c_open(mod->config.i2c_dev.c_str(), mod->config.i2c_addr);
+    if(fd == -1){
+        EVLOG_error << "evse_board_supportImpl::init:  i2c_open() failed: " << fd; 
+    }
+    else{
+        EVLOG_info << "evse_board_supportImpl::init:  i2c_open() success: " << fd; 
+    }
 }
 
 void evse_board_supportImpl::ready() {
--- modules/HardwareDrivers/EVSE/RaspiEvseBSP/board_support/evse_board_supportImpl.hpp
+++ modules/HardwareDrivers/EVSE/RaspiEvseBSP/board_support/evse_board_supportImpl.hpp
@@ -56,6 +56,8 @@ private:
     // ev@3370e4dd-95f4-47a9-aaec-ea76f34a66c9:v1
     Everest::Thread i2c_read_thread_handle;
     void i2c_read_thread();
+
+    int fd;
     // ev@3370e4dd-95f4-47a9-aaec-ea76f34a66c9:v1
 };
 
